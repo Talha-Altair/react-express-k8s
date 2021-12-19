f651fefd5626df68771268d479c0075aec7c9527
Talha-Altair <cooltalha27@gmail.com>
2021-12-19 16:57:18 +0530

    Updates for 2021-12-19 16:57:18

---
+++ run.sh
@@ -0,0 +1,7 @@
+docker build -t talhaabdurrahman/mern-frontend ./frontend
+
+docker build -t talhaabdurrahman/mern-backend ./backend
+
+docker push talhaabdurrahman/mern-frontend
+
+docker push talhaabdurrahman/mern-backend
