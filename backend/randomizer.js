2afe35bd949f64729f5754950dfb904158c98a21
Talha-Altair <cooltalha27@gmail.com>
2021-12-19 12:57:36 +0530

    Updates for 2021-12-19 12:57:36

--- backend/randomizer.js
+++ backend/randomizer.js
@@ -3,7 +3,8 @@ function get_random_num(){
     let num = Math.floor(Math.random() * 10000);
 
     result = {
-        num: num
+        num: num,
+        status: "success"
     }
     
     return result;
