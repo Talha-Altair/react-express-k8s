9ee485ae84ac9752c5d1ecb4493ddaa6717cb358
Talha-Altair <cooltalha27@gmail.com>
2021-12-19 15:41:04 +0530

    Updates for 2021-12-19 15:41:04

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 const randomizer = require('./randomizer');
+const faker = require('faker');
 
 app.get('/', (req, res) => {
 
     let num = randomizer.get_random_num();
 
-    res.json(num);
+    var randomCard = faker.helpers.createCard();
+
+    result = {
+        "id": num,
+        "data": {
+            "name": randomCard.name,
+            "username": randomCard.username,
+            "email": randomCard.email,
+            "phone": randomCard.phone,
+        },
+    }
+
+    res.json(result);
 
 });
 
--- backend/randomizer.js
+++ backend/randomizer.js
@@ -3,8 +3,7 @@ function get_random_num(){
     let num = Math.floor(Math.random() * 10000);
 
     result = {
-        num: num,
-        status: "success"
+        num: num
     }
     
     return result;
