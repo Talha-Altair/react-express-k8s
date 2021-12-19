21e2be8e15af2772209f3dc7d7ffaee557e57840
Talha-Altair <cooltalha27@gmail.com>
2021-12-19 12:27:16 +0530

    Updates for 2021-12-19 12:27:16

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+
+    res.sendFile(__dirname + '/views/index.html');
+
+});
+
+app.listen(port, () => {
+
+    console.log('Listening on port ' + port);
+
+});
